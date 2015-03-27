5d9549b26799623dd8870d8fc8b4dbc843c2ce40
Alexander von Below <Alex@vonBelow.Com>
2015-03-27 10:37:48 +0100

    Initial Commit

---
+++ C++Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  C++Test
+//
+//  Created by Alexander v. Below on 27.03.15.
+//  Copyright (c) 2015 Alexander von Below. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
